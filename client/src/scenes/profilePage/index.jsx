f43d0a78f9977786003b7753a3a8fc7c4badfc06
Gordon <gohuang4@gmail.com>
2023-11-21 22:02:09 -0800

    readme update

--- client/src/scenes/profilePage/index.jsx
+++ client/src/scenes/profilePage/index.jsx
@@ -1,12 +1,12 @@
-import { Box, useMediaQuery } from "@mui/material"
-import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
-import { useParams } from "react-router-dom"
-import Navbar from "scenes/navbar"
-import FriendListWidget from "scenes/widgets/FriendListWidget"
-import MyPostWidget from "scenes/widgets/MyPostWidget"
-import PostsWidget from "scenes/widgets/PostsWidget"
-import UserWidget from "scenes/widgets/UserWidget"
+import { Box, useMediaQuery } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Navbar from "scenes/navbar";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import UserWidget from "scenes/widgets/UserWidget";
 import React from 'react';
 
 const ProfilePage = () => {
@@ -26,7 +26,7 @@ const ProfilePage = () => {
 
     useEffect(() => {
         getUser()
-    }, [])
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
     if (!user) return null
 
